bd043afad0260a00ca0ef249442ef61d6334ba01
cuong0906 <cuong090694@gmail.com>
2024-02-07 21:19:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! Lớp deooo :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
